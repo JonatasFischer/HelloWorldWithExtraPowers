05a2a6858fe9df03396ee61ee8333b3c89161fc7
Jonatas Fischer <jonatas.fischer@sportradar.com>
2022-08-24 18:36:50 +0200

    cleaned up unused tests

--- src/test/java/com/example/greeting/RandomGreeterPickerTest.java
+++ src/test/java/com/example/greeting/RandomGreeterPickerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import java.util.List;
+import java.util.Random;
 import java.util.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,10 +16,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class RandomGreeterPickerTest {
 
   @Test
-  void GivenAPickerHasOnlyOneGreeterThatOneShouldAlwaysBePicked(@Mock Greeter hello,
-                                                                @Mock RandomGenerator random) {
-    var picker = new RandomGreeterPicker(List.of(hello), random);
-    when(random.nextInt(1)).thenReturn(0);
+  void GivenAPickerHasOnlyOneGreeterThatOneShouldAlwaysBePicked(@Mock Greeter hello) {
+    var picker = new RandomGreeterPicker(List.of(hello), new Random());
     when(hello.greet()).thenReturn("Hello");
     assertEquals("Hello", picker.pick().greet());
   }
